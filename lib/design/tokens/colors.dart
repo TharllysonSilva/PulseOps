9aca14b65ca24ab56e84df5483d4406b9d2074e7
Tarllysson Silva <tarllyssondesigner@gmail.com>
2026-02-21 20:47:59 -0300

    design system

---
+++ lib/design/tokens/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF5B6CFF);
+  static const secondary = Color(0xFF00C2FF);
+
+  static const background = Color(0xFFF6F7FB);
+  static const surface = Colors.white;
+
+  static const textPrimary = Color(0xFF111827);
+  static const textSecondary = Color(0xFF6B7280);
+
+  static const success = Color(0xFF22C55E);
+  static const warning = Color(0xFFF59E0B);
+  static const danger  = Color(0xFFEF4444);
+}
